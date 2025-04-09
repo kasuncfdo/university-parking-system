8e0eb490e6e3fc0ac28c0df0224cfb0e6aa2dfe9
imeshavindi <imeshaa2004@gmail.com>
2025-04-09 22:39:22 +0530

    add method

---
+++ backend/java method/function.java
@@ -0,0 +1,11 @@
+
+public class Test {
+    public static void main(String[] args) {
+        printousl();
+    }
+
+    public static void printousl() {
+        System.out.println("Hello ");
+
+    }
+}
