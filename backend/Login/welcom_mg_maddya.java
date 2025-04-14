482b9c41513af51654d572ad46ed7bd698efed29
MaddyaDeZoysa <info.maddyadezoysa@gmail.com>
2025-04-14 23:00:59 +0530

    Create welcome mg

---
+++ backend/Login/welcom_mg_maddya.java
@@ -0,0 +1,19 @@
+public class Welcomemg {
+    static void Welcome(){
+        System.out.println("\n \uD83D\uDE97 Welcome University Parking System project \uD83C\uDFEB \n");
+        System.out.println("\n" +
+                "   ____  _    _  _____ _      \n" +
+                "  / __ \\| |  | |/ ____| |     \n" +
+                " | |  | | |  | | (___ | |     \n" +
+                " | |  | | |  | |\\___ \\| |     \n" +
+                " | |__| | |__| |____) | |____ \n" +
+                "  \\____/ \\____/|_____/|______|\n" +
+                "                              \n" +
+                "                              \n");
+    }
+
+    public static void main(String[]args){
+
+        Welcome();
+    }
+}
