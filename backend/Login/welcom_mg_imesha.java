36aeef6f848f5ee9b5b99e178e719600ca3dcacc
imeshavindi <imeshaa2004@gmail.com>
2025-04-19 20:39:35 +0530

    update welcomemg

---
+++ backend/Login/welcom_mg_imesha.java
@@ -0,0 +1,17 @@
+public class Welcomemg {
+    static void Welcome(){
+                System.out.println("\n \uD83D\uDE97 Welcome to the University Parking System Project \uD83C\uDFEB \n");
+                System.out.println("\n" +
+                        "  _       __     __                             __          \n" +
+                        " | |     / /__  / /________  ____ ___  ___     / /_____     \n" +
+                        " | | /| / / _ \\/ / __/ __ \\/ __ `_ \\/ _ \\   / __/ __ \\  \n" +
+                        " | |/ |/ /  _/ / // // / / / / / /  _/  / // /_/ /     \n" +
+                        " |/|/\\//\\/\\// // //\\/   \\/\\_/ \n" +
+                                                                      \n")
+    }
+    public static void main(String[]args){
+
+        Welcome();
+    }
+}
+)
